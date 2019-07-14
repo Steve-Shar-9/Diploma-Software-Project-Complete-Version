84a0932288a4541dfb83dc6c1226b1d3ec79e93b
Noah Yek <ninjayek@gmail.com>
2019-07-14 13:56:15 +0800

    new update 1.2.1

--- App.js
+++ App.js
@@ -93,7 +93,7 @@ const MyDrawerNavigator = createDrawerNavigator({
   }
 },
   {
-    initialRouteName: 'AdminAnnouncement',
+    initialRouteName: 'Main',
     contentComponent: SideMenu,
     drawerWidth: 300
   }
