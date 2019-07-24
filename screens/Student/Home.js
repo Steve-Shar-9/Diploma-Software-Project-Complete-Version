405d393173284fccce72cd846f193171081054dc
Noah Yek <ninjayek@gmail.com>
2019-07-24 12:47:17 +0800

    update

--- screens/Student/Home.js
+++ screens/Student/Home.js
@@ -313,6 +313,14 @@ const styles = StyleSheet.create({
     height: 200,
     elevation: 2,
     borderRadius: 10,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 7,
+    },
+    shadowOpacity: 0.43,
+    shadowRadius: 9.51,
+    elevation: 15,
   },
 
   centerHeader: {
